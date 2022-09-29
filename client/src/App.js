076666daf717195345ba95ade9c18ab5a547dafb
VinicinBraga <vinicius@bragamatta.com.br>
2022-09-29 11:40:24 -0300

    CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Title</h1>
+      <div className="main-box">
+        <label>Food Name:</label>
+        <input type="text"></input>
+        <label>Food Name:</label>
+        <input type="number"></input>
+      </div>
+      <button className="add-btn">Add To List</button>
     </div>
   );
 }
