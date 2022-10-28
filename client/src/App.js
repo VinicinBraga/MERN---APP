5c89af8de5d1e3586f63964a9bd7a7561f23b7d5
VinicinBraga <vinicius@bragamatta.com.br>
2022-10-28 17:15:21 -0300

    Updating Css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "./asssets/images/logo.png";
 
 import "./App.css";
 
@@ -6,15 +7,83 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <h1>CUSTOMERS DATA</h1>
+        <img className="logo" src={logo} alt="logo"></img>
+        <div className="title">
+          <h1>CUSTOMERS</h1>
+        </div>
       </div>
       <div className="registration-container">
-        <div className="registration">
+        <div className="register-left">
           <h3>Registration</h3>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>Name:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer name" />
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>Phone:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer phone" />
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>E-mail:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer e-mail" />
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label className="address-title">Address</label>
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>Zip Code:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer ZIP code" />
+            </div>
+          </div>
+
+          <div className="register-tag">
+            <div className="register-label">
+              <label>Street:</label>
+            </div>
+            <div className="register-input-row">
+              <input id="street" type="text" placeholder="Customer Street" />
+              <input id="number" type="text" placeholder="Number" />
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>City:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer City" />
+            </div>
+          </div>
+          <div className="register-tag">
+            <div className="register-label">
+              <label>Country:</label>
+            </div>
+            <div className="register-input">
+              <input type="text" placeholder="Customer Country" />
+            </div>
+          </div>
         </div>
         <div className="descripition">
-          <h3>Project Description</h3>
-          <input type="text" />
+          <h3>Description</h3>
+          <div>
+            <textarea type="text" />
+          </div>
           <div className="add-btn-container">
             <button>Add to list</button>
           </div>
